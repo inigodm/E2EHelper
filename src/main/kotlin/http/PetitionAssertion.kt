cbaad7e76893c6ba7ec41fa38a2a925280ac5654
inigodm <inigo.delgado@gmail.com>
2021-12-11 23:44:22 +0100

    Fix http utils

--- src/main/kotlin/http/PetitionAssertion.kt
+++ src/main/kotlin/http/PetitionAssertion.kt
@@ -4,53 +4,67 @@ import okhttp3.*
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 
-interface Petition<T> {
+class Petition(val url: String) {
+    val innerHeaders = mutableMapOf<String, String>()
+
+    fun withHeaders(headers: Map<String, String>) : Petition {
+        innerHeaders.putAll(headers)
+        return this
+    }
+
+    fun withHeader(name: String, value: String) : Petition {
+        innerHeaders[name] = value
+        return this
+    }
+
+    fun withContentType(contentType: String) : Petition {
+        innerHeaders["Content-Type"] = contentType
+        return this
+    }
+
+    fun sendAGet(): RestResponse {
+        val client = OkHttpClient()
+        val res = client.newCall(
+            createAPetition(url, innerHeaders)
+                .get()
+                .build()).execute()
+        return RestResponse.from(res)
+    }
+
+    fun sendAPut(body: String): RestResponse {
+        val client = OkHttpClient()
+        val res = client.newCall(
+            createAPetition(url, innerHeaders)
+                .put(body.toRequestBody())
+                .build()).execute()
+        return RestResponse.from(res)
+    }
+
+    fun sendAPost(body: String): RestResponse {
+        val client = OkHttpClient()
+        val res = client.newCall(
+            createAPetition(url, innerHeaders)
+                .post(body.toRequestBody())
+                .build()).execute()
+        return RestResponse.from(res)
+    }
+
+    fun sendADelete(body: String = ""): RestResponse {
+        val client = OkHttpClient()
+        val res = client.newCall(
+            createAPetition(url, innerHeaders)
+                .delete(body.toRequestBody())
+                .build()).execute()
+        return RestResponse.from(res)
+    }
+
     companion object {
-        @JvmStatic
-        @JvmOverloads
-        fun sendAGetTo(url: String, headers: Map<String, String> = mapOf()): RestResponse {
-            val client = OkHttpClient()
-            val res = client.newCall(
-                createAPetition(url, headers)
-                    .get()
-                    .build()).execute()
-            return RestResponse.from(res)
-        }
 
         @JvmStatic
-        @JvmOverloads
-        fun sendAPutTo(url: String, body: String, headers: Map<String, String> = mapOf()): RestResponse {
-            val client = OkHttpClient()
-            val res = client.newCall(
-                createAPetition(url, headers)
-                    .put(body.toRequestBody())
-                    .build()).execute()
-            return RestResponse.from(res)
-        }
+        fun to(url: String) = Petition(url)
 
         @JvmStatic
-        @JvmOverloads
-        fun sendAPostTo(url: String, body: String, headers: Map<String, String> = mapOf()): RestResponse {
-            val client = OkHttpClient()
-            val res = client.newCall(
-                createAPetition(url, headers)
-                    .post(body.toRequestBody())
-                    .build()).execute()
-            return RestResponse.from(res)
-        }
-
-        @JvmStatic
-        @JvmOverloads
-        fun sendADeleteTo(url: String, body: String = "", headers: Map<String, String> = mapOf()): RestResponse {
-            val client = OkHttpClient()
-            val res = client.newCall(
-                createAPetition(url, headers)
-                    .delete(body.toRequestBody())
-                    .build()).execute()
-            return RestResponse.from(res)
-        }
-
-        fun createAPetition(url: String, headers: Map<String, String>): Request.Builder {
+        private fun createAPetition(url: String, headers: Map<String, String>): Request.Builder {
             val request = Request.Builder()
             headers.forEach { request.header(it.key, it.value) }
             return request.url(url)
--- src/test/kotlin/http/HttpGetPetitionAssertionTest.kt
+++ src/test/kotlin/http/HttpGetPetitionAssertionTest.kt
@@ -7,54 +7,54 @@ import org.opentest4j.AssertionFailedError
 class HttpGetPetitionAssertionTest {
     @Test
     fun `should call to a page and assert that is 200`() {
-        Petition.sendAGetTo("http://localhost/get")
+        Petition.to("http://localhost/get").sendAGet()
             .assertThatResponseIsOk()
     }
 
     @Test
     fun `should throw assert error if error is not ok`() {
         assertThrows<AssertionFailedError> {
-            Petition.sendAGetTo("http://localhost/status/400")
+            Petition.to("http://localhost/status/400").sendAGet()
                 .assertThatResponseIsOk()
         }
     }
 
     @Test
     fun `should assert that a 400 is returned`() {
-        Petition.sendAGetTo("http://localhost/status/400")
+        Petition.to("http://localhost/status/400").sendAGet()
             .assertThatResponseCodeIs(400)
     }
 
     @Test
     fun `should respond with given values in the body`() {
         val response : Map<String, Any> = mapOf("url" to "http://localhost/get")
-        Petition.sendAGetTo("http://localhost/get")
+        Petition.to("http://localhost/get").sendAGet()
             .assertThatBodyContains(response)
     }
 
     @Test
     fun `should respond with given string in the body`() {
-        Petition.sendAGetTo("http://localhost/get")
+        Petition.to("http://localhost/get").sendAGet()
             .assertThatBodyContains("url")
     }
 
     @Test
     fun `should send a put petition with body and receive a 200`() {
-        Petition.sendAPutTo("http://localhost/put", "innerbody")
+        Petition.to("http://localhost/put").sendAPut("innerbody")
             .assertThatResponseIsOk()
             .assertThatBodyContains("innerbody")
     }
 
     @Test
     fun `should send a post petition with body and receive a 200`() {
-        Petition.sendAPostTo("http://localhost/post", "innerbody")
+        Petition.to("http://localhost/post").sendAPost("innerbody")
             .assertThatResponseIsOk()
             .assertThatBodyContains("innerbody")
     }
 
     @Test
     fun `should send a delete petition with body and receive a 200`() {
-        Petition.sendADeleteTo("http://localhost/delete", "innerbody")
+        Petition.to("http://localhost/delete").sendADelete("innerbody")
             .assertThatResponseIsOk()
             .assertThatBodyContains("innerbody")
     }
